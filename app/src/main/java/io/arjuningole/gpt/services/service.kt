b334fb64c8c299bb5d7219bc64cf2eb69cff9255
Arjun Ingole <arjuningole38@gmail.com>
2022-10-24 16:39:19 +0530

    [TEST] CD: Setup CD pipeline on actions
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- app/src/main/java/io/arjuningole/gpt/services/service.kt
+++ app/src/main/java/io/arjuningole/gpt/services/service.kt
@@ -3,9 +3,10 @@ package io.arjuningole.gpt.services
 import com.aallam.openai.api.completion.CompletionRequest
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
+import io.arjuningole.gpt.BuildConfig
 import kotlinx.coroutines.*
 
-val apiKey = ""
+val apiKey = BuildConfig.API_KEY;
 val openAI = OpenAI(apiKey)
 var story = "Say Hello! Maybe"
 
